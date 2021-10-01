a7136bf99baed8fc5b61cf34f5b5edbf12996214
Pierce <piercedavidraphael@gmail.com>
2021-10-01 11:47:36 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return ++n;
+}
+
+function decrement(n) {
+    return --n;
+}
+
+function makeInt(n) {
+    let parInt = parseInt(n, 10);
+    return parInt;
+}
+
+function preserveDecimal(n) {
+    let preDec = parseFloat(n);
+    return preDec;
+}
